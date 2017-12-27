890e9d0687d6a199298d36ae1c09c27356be405f
minimalista12 <32286745+minimalista12@users.noreply.github.com>
2017-12-27 00:47:15 -0300

    -ejercicio 10

---
+++ app.js
@@ -0,0 +1,11 @@
+/*EJERCICIO 10
+
+Escribe una funcion donde dado el nombre de la persona pueda retornar el numero de asiento que le corresponde. 
+Si la personas no esta en la lista o no tiene asiento, retornar "0"
+*/
+
+//Ingresamos el array con distintos elementos 
+var lista = ['mari', 'lisa', 'jana', 'dani', 'pepi'];
+//
+var a = lista.indexOf('lisa')+1;
+console.log(a)
