718b9c3a9b42ddb581ef8cc95b909597778919f5
minimalista12 <32286745+minimalista12@users.noreply.github.com>
2017-12-28 01:00:35 -0300

    -problema 13  a medias

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ console.log(indice)
 
 
 
-
 /*EJERCICIO 10
 
 Escribe una funcion donde dado el nombre de la persona pueda retornar el numero de asiento que le corresponde. 
@@ -21,13 +20,14 @@ Si la personas no esta en la lista o no tiene asiento, retornar "0"
 */
 
 //Ingresamos el array con distintos elementos 
-var lista = ['mari', 'lisa', 'jana', 'dani', 'pepi'];
+ var lista = ['mari', 'lisa', 'jana', 'dani', 'pepi'];
 
 //Le pedimos a indexOf que nos retorne la posicion de 'lisa', agregamos +1 para asegurarnos que cuenta desde 1 no 0 
-var a = lista.indexOf('lisa')+1;
+ var a = lista.indexOf('lisa')+1;
 
 //Deberia retornar 2
 console.log(a)
+
 
 
 /*EJERCICIO 12
@@ -44,5 +44,41 @@ function esPalindromo(str){
     //establecemos la funcion para que recorra la palabra y determine si es palindromo o no 
     return esPalindromo(str.slice(1,str.length-1));
 }
-//en el caso de 'palindromo' debe retornar "false"
+//Deberia retornar "false"
 console.log(esPalindromo("palindromo"));
+
+
+
+/*EJERCICIO 13
+
+Dado dos string (palabra1 y palabra2) escribir una función para determinar si palabra1 es anagrama de palabra2.
+Si es anagrama debe retornar true, en caso contrario false.
+*/
+
+ function esAnagrama (palabra1, palabra2) {
+    
+     if (palabra1.length !== palabra2.length) {
+        return false;    
+    }
+    
+    // 
+    var contador1 = {};
+    Array.prototype.forEach.call(palabra1, function(ch) {
+        contador1[ch] = contador1[ch] ? 1 + contador1[ch] : 1;
+    });
+
+    // 
+    var contador2 = palabra2.length;
+    for (var i = 0; i < contador2; i++) {
+        if(!contador1[palabra2[i]]) {
+            return false;
+        } else {
+            contador1[palabra2[i]] -= 1;
+        }
+    }
+    return true;
+}
+//Deberia retornar 'true'
+console.log(esAnagrama('amar','rama')); 
+
+
